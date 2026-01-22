8259a3707ac10ed72b6f239217acfddc7b6a09aa
Francesco <francesco2969@email.com>
2026-01-22 12:27:45 +0100

    mod2

--- app/src/main/java/banca/ContoBancario.java
+++ app/src/main/java/banca/ContoBancario.java
@@ -35,7 +35,7 @@ public class ContoBancario {
 
 
     public int prelievo(int quota) {
-        System.out.println("Prelievo quota:fffcw " + quota);
+        System.out.println("Prelievo quota:ffddfcw " + quota);
         if (this.saldo < quota)
             return 0;
         else
@@ -43,5 +43,7 @@ public class ContoBancario {
         return 1;
     }
 
-
+    public boolean hasSaldoPositivo() {
+        return saldo > 0;
+    }
 }
