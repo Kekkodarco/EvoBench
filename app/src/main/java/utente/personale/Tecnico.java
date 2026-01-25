38c0c4fe6332c4cfd157808ea6ddf203a58f7cf8
Francesco <francesco2969@email.com>
2026-01-25 22:16:52 +0100

    cdddffe

--- app/src/main/java/utente/personale/Tecnico.java
+++ app/src/main/java/utente/personale/Tecnico.java
@@ -17,7 +17,7 @@ public class Tecnico {
     public String getName() {return name;}
 
     public String getSurname() {
-        System.out.println("ddd");
+        System.out.println("ddddd");
         return surname;}
 
     public String getProfession() {return profession;}
